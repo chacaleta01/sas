6c51ab964bf881e4d0201e2aaae6fda5e1a95cb2
abderrahman abourial <abdouotaku36@gmail.com>
2025-09-05 15:57:36 +0100

    change Pi from float to an const

--- firstPDF/Q2.c
+++ firstPDF/Q2.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
+const Pi = 3.14;
 int main(){
 
     int r=10;
-    float v, Pi;
-    Pi = 3.14;
+    float v;
+    //Pi = 3.14;
     v = 4.0f/3.0f*Pi*(r*r*r);
-    printf("le volume du sphere est: %lf", v);
+    printf("le volume du sphere est: %.2f", v);
 
     return 0;
 }
