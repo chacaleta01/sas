ff68e63d474d3a5a5c174196a14ae247fbb1105c
abderrahman abourial <abdouotaku36@gmail.com>
2025-09-05 13:47:52 +0100

    added the second exercice

---
+++ firstPDF/Q2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+
+    int r=10;
+    float v, Pi;
+    Pi = 3.14;
+    v = 4.0f/3.0f*Pi*(r*r*r);
+    printf("le volume du sphere est: %lf", v);
+
+    return 0;
+}
