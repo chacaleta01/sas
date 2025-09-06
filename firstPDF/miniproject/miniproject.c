bbc024e4846b1f4e0d3c0bee8cd4dd12b843964e
abderrahman abourial <abdouotaku36@gmail.com>
2025-09-06 13:48:35 +0100

    i added and fixed some changes like added the student classe[30] to store the infos and fix the void saisirNotes()

--- firstPDF/miniproject/miniproject.c
+++ firstPDF/miniproject/miniproject.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
 void ajouterEtudiant();
+void saisirNotes();
 
-
-typedef struct student
+typedef struct 
 {
-    char *CNE;
-    char *Name;
-    char *Nickname;
-    float notes;
+    char CNE [20];
+    char Name[40];
+    char Nickname[40];
+    float notes[4];
     float moyenne;
+    
 } student;
 
+int nombreEtudients = 0;
+student classe[30];
 
 
 int main (){
-    int choisir;
+    int choisir ;
+    
+
     printf("1) Ajouter un étudiant\n2) Saisir les notes d’un étudiant\n3) Afficher tous les étudiants\n4) Afficher bulletin d’un étudiant\n5) Calculer la moyenne générale\n0) Quitter\n");
     printf("entrez un nombre de 0 a 5 :");
     scanf("%d",&choisir);
@@ -24,12 +29,16 @@ int main (){
     case 1:{
         ajouterEtudiant();
     }break;
-
+    case 2:{
+        saisirNotes();
+    }break;
     
     default: 
         break;
     }
 
+
+    //printf("le CIN d'etuent est: %s\nle nom d'etudient est: %s\nle prenom d'etudient est: %s", classe[nombreEtudients-1].CNE, classe[nombreEtudients-1].Name, classe[nombreEtudients-1].Nickname);
 
     return 0;
 }
@@ -37,16 +46,37 @@ int main (){
 
 
 void ajouterEtudiant(){
-    student abdou;
-
+    
+    
     printf("entrer le CNE/ID d'etudient : ");
-    scanf("%s",abdou.CNE);
+    scanf("%s",classe[nombreEtudients].CNE);
     
     printf("\nentrer le nom d'etudient : ");
-    scanf("%s",abdou.Name);
+    scanf("%s",classe[nombreEtudients].Name);
 
     printf("\nentrer le prenom d'etudient : ");
-    scanf("%s",abdou.Nickname);
+    scanf("%s",classe[nombreEtudients].Nickname);
 
+    nombreEtudients++;
+
+    main();
+    return ;
+}
+
+void saisirNotes(){
+    
+    int sommeNotes, num=1;
+    for (int i = 0; i < 4; i++)
+    {
+        printf("entrez la note %d: de l'etudient %s %s:", num, classe[0].Name, classe[0].Nickname );
+        scanf("%f", classe[nombreEtudients-1].notes); 
+        sommeNotes += classe[nombreEtudients-1].notes[i];
+        num++;
+    }
+    main();
+    
+
+    
+    
     return ;
 }
