aa7733a3bb969e62319d11d50b6bbd54cb16c38b
abderrahman <abdouotaku36@gmail.com>
2025-09-08 14:55:57 -0700

    add first ex from 3rd PDF

---
+++ thirdPDF/Q1.c
@@ -0,0 +1,36 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(void) {
+    bool digit_seen[10] = {false};
+    bool digit_repeated[10] = {false};
+    int digit;
+    long n;
+
+    printf("Entrez un nombre : ");
+    scanf("%ld", &n);
+
+    while (n > 0) {
+        digit = n % 10;
+        if (digit_seen[digit])
+            digit_repeated[digit] = true;
+        else
+            digit_seen[digit] = true;
+
+        n /= 10;
+    }
+
+    bool found = false;
+    printf("Chiffre(s) répété(s) : ");
+    for (int i = 0; i < 10; i++) {
+        if (digit_repeated[i]) {
+            printf("%d ", i);
+            found = true;
+        }
+    }
+    if (!found)
+        printf("Aucun");
+    
+
+    return 0;
+}
